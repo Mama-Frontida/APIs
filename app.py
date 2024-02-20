5ea11e7ddf5ba5e81d66752b0da3e618b2ffb75e
DanroyNdungu <danroymwangi@gmail.com>
2024-02-20 20:17:17 +0300

    feat: implement the generate and YT api

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
-import requests
+from flask import Flask, jsonify, request
+from helpers import extract_keywords, query, searchYT
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route("/generate-text", methods=["POST"])
+@app.route("/",)
+def index():
+    return jsonify('Welcome to Frontida'), 200
+
+@app.route("/generateText", methods=["POST"])
 def generate():
-    # Validate the request body as JSON
-#   //  data = request.get_json()
-    text = "Hello, how are you?"
+    if request.method == 'POST':
+        input_text = str(request.form['text'])
+        response, statusCode = query(input_text)
 
-    # Make a POST request to the chatbot API's endpoint
-    url = "https://api-inference.huggingface.co/models/Njambi-M/gpt2-finetuned"
-    response = requests.post(url, headers={"Authorization": "Bearer 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"},
-                             json={"inputs": text})
-
-    ## Parse the response content as JSON
-    if response.status_code == 200:
-        generated_text = response.json()[0]['generated_text'].split('\n')[1].replace('[a]', '').replace('[q]', '').strip()
-        # Return the response text as the output
-        return jsonify({"generated_text": generated_text})
-    else:
-         return jsonify({"unexpected error": response.text}), response.status_code
+        ## Parse the response content as JSON
+        if statusCode == 200:
+            keywords = extract_keywords(input_text)
+            youtubeLinks = searchYT(', '.join(keywords))
+            return jsonify({"generated_text": response,"keywords":keywords, "youtubeLink": youtubeLinks}), statusCode
+        else:
+            return jsonify({"unexpected error": response}), statusCode
     
 if __name__ == "__main__":
     app.run(debug=False)
---
+++ helpers.py
@@ -0,0 +1,55 @@
+import requests
+from youtube_search import YoutubeSearch
+
+API_URL = "https://api-inference.huggingface.co/models/Danroy/MamaFrontida"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json={
+	"inputs": f"therapy: {payload}",
+	})
+	return response.json(), response.status_code
+
+
+def extract_keywords(text):
+    # Define a list of stop words
+    stop_words = set([
+        "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves",
+                    "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their",
+                    "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was",
+                    "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and",
+                    "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between",
+                    "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off",
+                    "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any",
+                    "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so",
+                    "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"
+                                ])
+
+    # Tokenize and filter out stop words and punctuation
+    words = text.split()
+    keywords = [word.lower() for word in words if word.lower() not in stop_words and word.isalpha()]
+
+    return keywords
+
+def searchYT(input,max_results=5):
+    results = YoutubeSearch(input, max_results=max_results).to_dict()
+
+    # Keywords that might indicate triggering content
+    triggering_keywords = ['sad', 'dies by suicide', 'death by suicide', 'self-harm', 'triggering']
+
+    # Filter out potentially triggering results
+    filtered_results = []
+    for result in results:
+        title = result['title'].lower()
+        # Check if any triggering keyword is in the title or description
+        if not any(keyword in title  for keyword in triggering_keywords):
+            filtered_results.append(result)
+
+    links = []
+    for v in filtered_results:
+        links.append('https://www.youtube.com' + v['url_suffix'])
+    
+    return links
+
+if __name__ == '__main__':
+    print(searchYT(input='postpartum - i feel sad after giving birth'))
