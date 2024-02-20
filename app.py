6d7a29fcd9e29f2326a59e5a030c754b1a959735
Sir Nelson II <67654295+MwangiNelson@users.noreply.github.com>
2024-02-20 22:14:33 +0300

    Update app.py -godly works

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from helpers import extract_keywords, query, searchYT
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app)
 
 @app.route("/",)
 def index():
